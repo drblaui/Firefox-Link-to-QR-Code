a15ab9a040000be9aeba49c9e645bd038afe7194
Alexander Rudolph <alexr30@web.de>
2020-12-22 19:29:31 +0100

    commenting

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+//Create a Context Action for every link
 browser.contextMenus.create({
     id: "create-qr-code-for-link",
     title: "Generate QR Code for this link",
@@ -6,6 +7,7 @@ browser.contextMenus.create({
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "create-qr-code-for-link") {
+        //Execute QR Code generation in current tab 
         browser.tabs.executeScript(tab.id, {
             code: `var loc = window.location.href; var link = "https://api.qrserver.com/v1/create-qr-code/?size=256x256&data=" + loc; window.open(link, 'QRCode', 'height=256, width=256');`
         });
--- toQR.js
+++ toQR.js
@@ -1,31 +1,39 @@
+// Because it would be stupid to make a QR Code of the website that makes QR Codes
 if (!(window.location.href).includes("api.qrserver.com")) {
 
+    //Every QR Code should be 256x256, because that's big enough
+    //TODO: Option to change QR Code size
     var baseLink = "https://api.qrserver.com/v1/create-qr-code/?size=256x256&data=";
 
+    // Append current URL as data
     var targetLink = baseLink + window.location.href;
 
 
+    //Create button
     var button = document.createElement('button');
     button.id = "linkToQRButton";
     button.innerHTML = "URL to QR Code";
-
-
+    //Revert all style changes on the website (since they look weird otherwise)
     button.style.cssText = "all: revert; position: fixed; top: 0; right: 0; z-index: 9998";
     button.onclick = () => { createDiv(targetLink); };
-
     document.body.append(button);
 }
 
 function createDiv(url) {
+    //Create div that is (hopefully) always on top of everything
     var div = document.createElement('div');
     div.style.cssText = "position: fixed; top: 0; right: 0; z-index: 9999";
+
+    //Close button
     var x = document.createElement('button');
     x.innerText = "X";
-    x.style.cssText = "all: revert; position: fixed; top: 256px; right: 256px; z-index: 100000; color:white !important; background-color: red !important; border-radius: 50%;";
+    x.style.cssText = "all: revert; position: fixed; top: 256px; right: 256px; z-index: 100000; color:white; background-color: red; border-radius: 50%;";
     x.onclick = () => {
         div.remove();
     }
     div.append(x);
+
+    //QR Code
     var img = document.createElement('img');
     img.src = url;
     div.append(img);
