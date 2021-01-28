d51d078b9c87d9bbbd6cf0c6a1ec52064173535d
Alexander Rudolph <alexr30@web.de>
2021-01-28 21:17:44 +0100

    moved QR Code generation into "createDiv" Will now always get the current link on pages that have mutable URLS withour reloading

--- toQR.js
+++ toQR.js
@@ -1,25 +1,26 @@
 // Because it would be stupid to make a QR Code of the website that makes QR Codes
 if (!(window.location.href).includes("api.qrserver.com")) {
-
-    //Every QR Code should be 256x256, because that's big enough
-    //TODO: Option to change QR Code size
-    var baseLink = "https://api.qrserver.com/v1/create-qr-code/?size=256x256&data=";
-
-    // Append current URL as data
-    var targetLink = baseLink + window.location.href;
-
-
     //Create button
     var button = document.createElement('button');
     button.id = "linkToQRButton";
     button.innerHTML = "URL to QR Code";
     //Revert all style changes on the website (since they look weird otherwise)
     button.style.cssText = "all: revert; position: fixed; top: 0; right: 0; z-index: 9998";
-    button.onclick = () => { createDiv(targetLink); };
+    button.onclick = () => {
+        createDiv();
+        //console.log("Test");
+    };
     document.body.append(button);
 }
 
-function createDiv(url) {
+
+function createDiv() {
+    //Every QR Code should be 256x256, because that's big enough
+    //TODO: Option to change QR Code size
+    var baseLink = "https://api.qrserver.com/v1/create-qr-code/?size=256x256&data=";
+
+    // Append current URL as data
+    var targetLink = baseLink + window.location.href;
     //Create div that is (hopefully) always on top of everything
     var div = document.createElement('div');
     div.style.cssText = "position: fixed; top: 0; right: 0; z-index: 9999";
@@ -35,7 +36,7 @@ function createDiv(url) {
 
     //QR Code
     var img = document.createElement('img');
-    img.src = url;
+    img.src = targetLink;
     div.append(img);
     document.body.append(div);
 }
